fd69d7e10b38204a56e67cd8fde72998ed8d6b39
Nishant <itsnishant2003@gmail.com>
2023-07-11 21:57:19 +0530

    this is my first animating website

---
+++ script.js
@@ -0,0 +1,24 @@
+gsap.to(".image",{
+    ease:Expo.easeInOut,
+    width:"100%",
+    stagger:3,
+    repeat:-1
+    
+}) 
+    
+
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    stagger:3,
+    top:0,
+    repeat:-1
+    
+})    
+gsap.to(".text h1",{
+    delay:3,
+    ease:Expo.easeInOut,
+    stagger:3,
+    top:"-110%",
+    repeat:-1
+    
+})    
